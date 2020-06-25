e5f5312186d4368ebae2774c715cc0f4cc3dc183
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-06-25 16:08:43 +0300

    3.11: Lisätty static-middleware backendiin.

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ morgan.token('data', function (req, res) {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :data'))
+app.use(express.static('build'))
 
 // Kaikki puhelinnumerot
 app.get('/api/persons', (request, response) => {
