ec76bee2c416bdb9a3748aa772eb76ba117ff967
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-06-25 15:57:06 +0300

    deploy:full

---
+++ build/precache-manifest.0a2e14658912008fb68a51bb4751130a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01020999d816676332a4388c26f6b54e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ce9c1fc4ca871fdf0c",
+    "url": "/static/css/main.0bb661c4.chunk.css"
+  },
+  {
+    "revision": "8067308f561f86c5b4cd",
+    "url": "/static/js/2.b9612366.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b9612366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ce9c1fc4ca871fdf0c",
+    "url": "/static/js/main.d64d7a2a.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
