edf2f262ad24056c67e9d420b3e128a20ad9d82c
Iiro Kiviluoma <iiro.kiviluoma@tuni.fi>
2020-06-26 11:51:25 +0300

    3.14 valmis: Henkilöiden lisääminen tietokantaan onnistuu frontendin kautta. Poistettu kovakoodatut puhelinnumerot koodista.

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// Kovakoodatut puhelinnumerot
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -47,62 +23,37 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-// Yksittäinen puhelinnumero
+// Yksittäinen puhelinnumero, EI KÄYTÖSSÄ FRONTENDIN TOIMESTA!
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  //console.log(`Request on person with id: ${id}`)
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {  // Mikäli henkilöä ei löytynyt
-    //console.log(`Requested person not found.`)
-    response.status(404).end()
-  }
+  // Haetaan yksittäinen henkilö tietokannasta id:n avulla.
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
 })
-
-// Generoi numerolle uuden id:n käyttäen Math.random-funktiota.
-const generateId = () => {
-  const max = 9999
-  let newId = 1 + Math.floor(Math.random() * max)
-  //console.log(`Generated new id: ${newId}`)
-
-  // Generoidaan kunnes löytyy vapaa id.
-  while (persons.find(p => p.id === newId)) {
-    //console.log(`Id already present, generating new one...`)
-    newId = Math.floor(Math.random() * max)
-    //console.log(`Generated new id: ${newId}`)
-  }
-
-  return newId
-}
 
 // Yksittäinen lisäys
 app.post('/api/persons', (request, response) => {
-  //console.log(`Request on adding a new person.`)
   const reqBody = request.body
 
   if (!reqBody.name || !reqBody.number) {
     return response.status(400).json({
-      error: "Bad request!"
+      error: "Bad request: content missing."
     })
   }
 
-  if (persons.find(p => p.name === reqBody.name)) {
-    return response.status(400).json({
-      error: "Name already in the server."
-    })
-  }
-
-  const newPerson = {
+  const newPerson = new Person ({
     name: reqBody.name,
-    number: reqBody.number,
-    id: generateId()
-  }
+    number: reqBody.number
+  })
 
-  //console.log(newPerson)
-  persons = persons.concat(newPerson)
-  response.json(newPerson)
+  newPerson.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 // Yksittäinen poisto
@@ -112,9 +63,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
   // Poistettavaa henkilöä ei löydy.
   if (!persons.some(p => p.id === id)) {
-    response.status(400).json({
-      error: "Person not found."
-    })
+    response.status(404).end()
   }
 
   // Onnistunut poisto.
